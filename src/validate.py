0d12b2d274f5fc83cb9b57f5bdf5441452def7c2
blasevich <gagliano@hacari.org>
2025-01-16 16:37:51 +0100

    consider max and min sequence length

--- src/validate.py
+++ src/validate.py
@@ -4,53 +4,58 @@ import json
 D = {}
 
 def find_tag(str, tags): # find a specific tag in a string
-    found = 0
+    found = False
     i = 0
-    while not found and i<len(tags):
+    l = len(tags)
+    while not found and i<l:
         if tags[i] in str:
-            found = 1
+            found = True
             tag = tags[i]
         i += 1
 
     return tag
 
-def fun(tag, dict, file, out, threshold):
+def fun(tag, dict, file, out, threshold, max_length, min_length):
     sequences = 0
+    actual_sequences = 0
 
     for stream in dict:
         #print(stream)
-        D[tag]['num_sequences'] += 1 # keep track of the number of tls content type sequences
+        length = len(dict[stream]['sequence'])
 
-        dict[stream].pop('sequence')
-        sequences += 1
+        if length>=min_length and length<=max_length: # if len sequence <min or >max => dont consider this flow
+            D[tag]['num_sequences'] += 1
+            actual_sequences += 1
+            #print(dict[stream]['sequence'])
+            dict[stream].pop('sequence')
+            sequences += 1
 
-        print(dict[stream])
-        m = max(zip(dict[stream].values(), dict[stream].keys()))
-        print(m)
+            print(dict[stream])
+            m = max(zip(dict[stream].values(), dict[stream].keys()))
+            print(m)
 
-        if m[0] <= threshold:
-            print("classified as: NONE.", end=" ")
-            out.write("{} {} {} {} ".format(file, stream, dict[stream], "NONE"))
-            if "NONE" in file:
-                print("OK")
-                out.write("OK\n")
+            if m[0] <= threshold:
+                #print("classified as: NONE.", end=" ")
+                out.write("{} {} {} {} ".format(file, stream, dict[stream], "NONE"))
+                if "NONE" in file:
+                    #print("OK")
+                    out.write("OK\n")
+                else:
+                    #print("NOPE")
+                    out.write("NOPE\n")
             else:
-                print("NOPE")
-                out.write("NOPE\n")
-        else:
-            print("classified as: {}.".format(m[1]), end=" ")
-            out.write("{} {} {} {} ".format(file, stream, dict[stream], m[1])) 
-            #20211013_dridex 3 {'dridex': 0.057692307692307696, 'trickbot': 0.05589827058997696} dridex OK
-            if m[1] in file:
-                print("OK")
-                D[tag]['true'] += 1
-                out.write("OK\n")
-            else:
-                print("NOPE")
-                D[tag]['false'] += 1
-                out.write("NOPE\n")
+                #print("classified as: {}.".format(m[1]), end=" ")
+                out.write("{} {} {} {} ".format(file, stream, dict[stream], m[1])) 
+                if m[1] in file:
+                    #print("OK")
+                    D[tag]['true'] += 1
+                    out.write("OK\n")
+                else:
+                    #print("NOPE")
+                    D[tag]['false'] += 1
+                    out.write("NOPE\n")
 
-    return sequences
+    return actual_sequences
 
 def main():
     config_file = "../maltls.toml"
@@ -71,13 +76,15 @@ def main():
 
     results_file_all = data['validate']['results_file_all']
     threshold = data['threshold']
+    MAX = data['max_length']
+    MIN = data['min_length']
 
     sequences = 0 #double check number of sequences
 
     with open(results_file_all, 'w') as out:
         for file in R:
             print(file)
-            sequences = sequences + fun(find_tag(file, tags), R[file], file, out, threshold)
+            sequences = sequences + fun(find_tag(file, tags), R[file], file, out, threshold, MAX, MIN)
 
     print(D)
     print("\nnumber of sequences: {}".format(sequences))
